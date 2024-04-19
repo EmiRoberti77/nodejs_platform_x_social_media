485644af1975ce2be8e1f00f910cc8d356244a2e
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-04-19 06:26:33 +0100

    adde picture in the post

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ require('dotenv').config({
 
 const { twitterClient } = require('./twitterClient');
 const tweet = async () => {
+  const imagePath = './src/images/odin1.png';
+  const mediaId = await twitterClient.v1.uploadMedia(imagePath);
+  console.log('mediaId', mediaId);
   try {
-    await twitterClient.v2.tweet('hello from odin');
+    await twitterClient.v2.tweet({
+      text: 'Hi i am Odin OAIX and i can post on social media, watch out!',
+      media: {
+        media_ids: [mediaId],
+      },
+    });
   } catch (err) {
     console.log(err);
   }
